0233b60a6cab794c507b0f88ad2c16f74814e059
ashe <asiasheann@gmail.com>
2022-08-03 09:34:48 +0800

    customize eva tokyonight

--- lua/tokyonight/config.lua
+++ lua/tokyonight/config.lua
@@ -18,21 +18,21 @@ end
 config = {
   style = opt("style", "storm"),
   dayBrightness = opt("day_brightness", 0.3),
-  transparent = opt("transparent", false),
+  transparent = opt("transparent", true),
   commentStyle = opt("italic_comments", true) and "italic" or "NONE",
   keywordStyle = opt("italic_keywords", true) and "italic" or "NONE",
   functionStyle = opt("italic_functions", false) and "italic" or "NONE",
   variableStyle = opt("italic_variables", false) and "italic" or "NONE",
   hideInactiveStatusline = opt("hide_inactive_statusline", false),
   terminalColors = opt("terminal_colors", true),
-  sidebars = opt("sidebars", {}),
+  sidebars = opt("sidebars", {"qf", "vista_kind", "terminal", "packer"}),
   colors = opt("colors", {}),
   dev = opt("dev", false),
   darkFloat = opt("dark_float", true),
   darkSidebar = opt("dark_sidebar", true),
-  transparentSidebar = opt("transparent_sidebar", false),
+  transparentSidebar = opt("transparent_sidebar", true),
   transform_colors = false,
-  lualineBold = opt("lualine_bold", false),
+  lualineBold = opt("lualine_bold", true),
 }
 
 if config.style == "day" then
